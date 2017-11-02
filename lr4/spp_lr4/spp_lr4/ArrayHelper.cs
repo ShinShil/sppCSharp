02812b12832f631a85bd6369833c74266533393b
Nikita Starostin <n.starostin@itransition.com>
2017-11-02 15:13:02 +0300

    [lr4] lr4 ready

--- lr4/spp_lr4/spp_lr4/ArrayHelper.cs
+++ lr4/spp_lr4/spp_lr4/ArrayHelper.cs
@@ -8,5 +8,27 @@ namespace spp_lr4
 {
     class ArrayHelper
     {
+        public unsafe static int Sum(int[] arr, int count)
+        {
+            unsafe
+            {
+                fixed (int* arrPointer = &arr[0])
+                {
+                    int res = 0;
+                    for (int i = 0; i < count; ++i)
+                    {
+                        if (Int32.MaxValue - res > arrPointer[i])
+                        {
+                            res += arrPointer[i];
+                        }
+                        else
+                        {
+                            throw new OverflowException("Int overflow");
+                        }
+                    }
+                    return res;
+                }
+            }
+        }
     }
 }
--- lr4/spp_lr4/spp_lr4/Program.cs
+++ lr4/spp_lr4/spp_lr4/Program.cs
@@ -10,6 +10,19 @@ namespace spp_lr4
     {
         static void Main(string[] args)
         {
+            int[] arrOverflow = new int[] { int.MaxValue, 2 };
+            int[] arrOk = new int[] { 1, 2 };
+
+            try
+            {
+                Console.WriteLine(ArrayHelper.Sum(arrOk, arrOk.Length));
+                Console.WriteLine(ArrayHelper.Sum(arrOk, arrOk.Length + 4));
+                Console.WriteLine(ArrayHelper.Sum(arrOverflow, arrOk.Length + 2));
+            }
+            catch (OverflowException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
         }
     }
 }
