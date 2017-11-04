41962d3caf7d1e3c0101e87cd7d75d530687c1db
Nikita Starostin <n.starostin@itransition.com>
2017-11-04 16:16:28 +0300

    [lr5] basic markup

--- lr5/spp_lr5/spp_lr5/Program.cs
+++ lr5/spp_lr5/spp_lr5/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Threading;
 
 namespace spp_lr5
 {
@@ -10,7 +11,39 @@ namespace spp_lr5
     {
         static void Main(string[] args)
         {
+            if (!InputArgsAreValid(args)) return;
+            CopyFile(args[0], args[1]);
+        }
 
+        static void CopyFile(string src, string dest)
+        {
+            CopyFile(src, dest);
+        }
+
+        static bool InputArgsAreValid(string[] args)
+        {
+            bool res = true;
+            if (args.Length < 3)
+            {
+                PrintUsage();
+                res = false;
+            }
+            else
+            {
+                int nThreads = 0;
+                if (!Int32.TryParse(args[2], out nThreads))
+                {
+                    Console.WriteLine("3rd arg should an integer");
+                    PrintUsage();
+                    res = false;
+                }
+            }
+            return res;
+        }
+
+        static void PrintUsage()
+        {
+            Console.WriteLine("Wrong input, usage:\n\tspp_lr5 <src_file> <dest_file> <thread_amount>\n");
         }
     }
 }
